9738e8e36d93fce9c23e68ee79d76974d9978fc7
Kontham Bhanu Prasad Reddy <bhanukontham1109@gmail.com>
2026-01-12 13:02:57 +0530

    Database connection established

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
+require("dotenv").config();
+console.log("DB URL:", process.env.DATABASE_URL);
+
 const express = require("express");
+const pool = require("./utils/db");
+
 const app = express();
+//DB TEST
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) console.error("DB Error:", err);
+  else console.log("DB Connected:", res.rows[0]);
+});
 
 app.use(express.json());
 
